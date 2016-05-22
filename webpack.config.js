1f95b2e5c774564717756e28b3a4b773d2faa6ec
Alex Gian <alexhgian@gmail.com>
2016-05-21 22:52:33 -0700

    Gulp webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,29 @@
+var webpack = require("webpack");
+
 module.exports = {
-  entry: "./client-webpack/app.js",
+  entry: __dirname + "/client-webpack/app.js",
   cache: false,
   output: {
-    path: './client/app/',
+    path: __dirname + '/client/app/',
     filename: "bundle.js"
   },
   module: {
     loaders: [
-      {test: /\.ejs$/, loader: 'ejs-compiled'}
+      {test: /\.ejs$/, loader: 'ejs-compiled?htmlmin'}
     ]
   },
   'ejs-compiled-loader': {
-
-  }
+      'beautify': false,
+      'htmlmin': true, // or enable here
+      'htmlminOptions': {
+        removeComments: true
+      }
+  },
+  plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+          compress: {
+              warnings: false
+          }
+      })
+  ]
 }
